8125952fcd23e55a51f9ed86dc85a037463cb568
DHUSHYANDH <147026111+dhushyandh@users.noreply.github.com>
2026-09-22 18:27:43 +0530

    Add Google authentication controller

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/User';
 import { generateToken, AuthenticatedRequest } from '../middleware/auth';
 import { env } from '../config/env';
